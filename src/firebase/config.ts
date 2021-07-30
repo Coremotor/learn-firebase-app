a7b7f3b4e10eb2aa8267b64aa078a823a3ebd17f
Coremotor <coremotor@users.noreply.github.com>
2021-07-30 12:49:02 +0300

    start

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,12 +4,12 @@ import 'firebase/analytics'
 import 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD6CECstm_yDPNlrHANLhUja7szr0KbM_o',
-  authDomain: 'learn-firebase-app-34657.firebaseapp.com',
-  projectId: 'learn-firebase-app-34657',
-  storageBucket: 'learn-firebase-app-34657.appspot.com',
-  messagingSenderId: '669810222194',
-  appId: '1:669810222194:web:5f6613b61364c908bdceeb',
+  apiKey: process.env.REACT_APP_FB_API_KEY,
+  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FB_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FB_APP_ID,
 }
 
 firebase.initializeApp(firebaseConfig)
